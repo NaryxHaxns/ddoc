3cb66e7255071e582768d07922ee5dce69613fe3
Ryan Nash <nashkbry@gmail.com>
2021-08-17 18:22:35 -0700

    created the Nav component

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './nav.css'
+
+export default function nav() {
+    return(
+        <div className="nav">
+
+        </div>
+    )
+}
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './landing.css'
+import Nav from '../../components/nav/nav'
 
 export default function landing() {
     return(
         <div className="landing">
-
+            <Nav />
         </div>
     )
 }
