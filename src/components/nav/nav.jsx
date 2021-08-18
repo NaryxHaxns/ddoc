ddd71dbcfa7b0980ef6ca6366e45085ac2225c67
Ryan Nash <nashkbry@gmail.com>
2021-08-17 18:51:25 -0700

    selected font for company name in nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,7 +4,7 @@ import './nav.css'
 export default function nav() {
     return(
         <div className="nav">
-
+            <p className="companyName">Dragon Designs of California</p>
         </div>
     )
 }
